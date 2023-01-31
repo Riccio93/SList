d106d8d23e7efeca752e6178c5ee027ddd7ea364
Andrea Ricci <andrearicci500@gmail.com>
2023-01-31 22:11:47 +0100

    First commit

---
+++ RicciSList/RicciSList.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SList.h"
+
+int main()
+{
+    SList<int> list;    
+
+    std::cout << "Hello World!\n";
+}
---
+++ RicciSList/SList.h
@@ -0,0 +1,29 @@
+#pragma once
+
+template<typename T>
+class SList
+{
+private:
+
+	struct Node
+	{
+		T value;
+		Node* next;
+
+		//Node constructors and destructor
+		Node() : next(nullptr) {}
+		Node(T in_value, Node* in_next) : value(in_value), next(in_next) {}
+		virtual ~Node() { if (next) delete next; }
+	};
+
+	Node* head;
+
+public:
+
+	//List constructors and destructor
+	explicit SList() : head(nullptr) { }
+	explicit SList(std::size_t size) : head(nullptr) { }
+	explicit SList(std::size_t size, const T& val) { }
+	//etc...
+
+};
