69e02136628b0c5aded622a9c268267a99b9df7e
Andrea Ricci <andrearicci500@gmail.com>
2023-02-11 18:06:41 +0100

    Add iterator struct

--- RicciSList/RicciSList.cpp
+++ RicciSList/RicciSList.cpp
@@ -3,7 +3,19 @@
 
 int main()
 {
-    SList<int> list;    
+    SList<int> list;   
 
-    std::cout << "Hello World!\n";
+    std::cout << "Is the list empty? " << list.empty() << std::endl;
+
+    std::cout << list.front() << std::endl;
+
+    //Push front test
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+
+    int num = list.front();
+    std::cout << num << std::endl;
+
+    std::cout << "Is the list empty? " << list.empty() << std::endl;    
 }
--- RicciSList/SList.h
+++ RicciSList/SList.h
@@ -1,18 +1,28 @@
 #pragma once
 
+#include <vector>
+
+using std::cout;
+using std::endl;
+
 template<typename T>
 class SList
 {
+
+	//Aliases
+	using value_type = T;
+	using size_type = std::size_t;
+
 private:
 
 	struct Node
 	{
-		T value;
+		value_type value;
 		Node* next;
 
 		//Node constructors and destructor
 		Node() : next(nullptr) {}
-		Node(T in_value, Node* in_next) : value(in_value), next(in_next) {}
+		Node(value_type in_value, Node* in_next) : value(in_value), next(in_next) {}
 		virtual ~Node() { if (next) delete next; }
 	};
 
@@ -20,20 +30,83 @@ private:
 
 public:
 
+	template<typename T>
+	struct SListIterator
+	{
+	private:
+		Node* node;
+
+	public:
+		friend class SList;
+
+		//Iterator constructors and destructor
+		SListIterator() : node(nullptr) {}
+		SListIterator(Node* inNode) : node(inNode) {}
+		SListIterator(const SListIterator& other) : node(other.head) {}
+		virtual ~SListIterator() {};
+
+		//Operators
+		SListIterator& operator++()
+		{
+			if (node)
+				node = node->next;
+			return *this;
+		}
+
+		bool operator==(const SListIterator& other)
+		{
+			return node == other.node;
+		}
+
+		bool operator!=(const SListIterator& other)
+		{
+			return node != other.node;
+		}
+
+		value_type& operator*()
+		{
+			return (*node).value;
+		}
+
+		value_type* operator->()
+		{
+			return &(head->value);
+		}
+	};
+
 	//List constructors and destructor
 	explicit SList() : head(nullptr) { }
-	explicit SList(std::size_t size) : head(nullptr) { }
-	explicit SList(std::size_t size, const T& val) { }
-	//etc...
+	explicit SList(size_type size) : head(nullptr) { }
+	explicit SList(size_type size, const value_type& val)
+	{
 
-	void empty();
-	void clear();
+	}
+	virtual ~SList() {};
+	//TODO: etc...
+
+	inline value_type front() const
+	{	
+		if (empty())
+		{
+			cout << "ERROR: called front(), but list is empty." << endl;
+			return NULL;
+		}
+		return head->value;
+	}
+
+	inline void push_front(value_type in_value)
+	{
+		Node* newNode = new Node;
+		newNode->value = in_value;
+		newNode->next = head;
+		head = newNode;
+	}
+	
+	inline bool empty() const
+	{
+		return head == nullptr;
+	}
+
+	/*void empty();
+	void clear();*/
 };
-
-template<typename T>
-inline void SList<T>::empty()
-{ }
-
-template<typename T>
-inline void SList<T>::clear()
-{ }
